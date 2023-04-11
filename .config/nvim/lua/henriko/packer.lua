b36f3b1bc6859a4f8c2caa9641dc405b67a01f04
Henrik Schwarz <henrik@schwrz.dk>
2023-04-11 10:03:35 +0200

    updated bashrc, started tmux, created neovim config

---
+++ .config/nvim/lua/henriko/packer.lua
@@ -0,0 +1,60 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use ({ 
+		'ray-x/aurora',
+		as = 'aurora',
+		config = function()
+			-- set termguicolors = set termguicolors
+			vim.opt.termguicolors = true        -- 24 bit color
+			vim.g.aurora_italic = 1          -- italic
+			vim.g.aurora_transparent = 1     -- transparent
+			vim.g.aurora_bold = 1            -- bold
+			vim.g.aurora_darker = 1          -- darker background
+		end
+	})
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
+
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+			end,
+		},
+		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},     -- Required
+		{'hrsh7th/cmp-nvim-lsp'}, -- Required
+		{'L3MON4D3/LuaSnip'},     -- Required
+
+        }
+    }
+
+    use 'mfussenegger/nvim-jdtls'
+
+  end)
