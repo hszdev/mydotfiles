58ba4adfd4ba5f25c17b83c6d86d05a6599219ce
Henrik <henrik@schwrz.dk>
2023-10-14 19:13:35 +0200

    fixed telescope version

--- .config/nvim/lua/henriko/packer.lua
+++ .config/nvim/lua/henriko/packer.lua
@@ -8,9 +8,9 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+      'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
     }
 
     use ({
@@ -26,7 +26,13 @@ return require('packer').startup(function(use)
         end
     })
 
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
     use('nvim-treesitter/playground')
 
     use('theprimeagen/harpoon')
